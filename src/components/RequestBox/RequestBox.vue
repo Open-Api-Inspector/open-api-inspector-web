4ed0b6dee5b67274c83d04c77c61347573f14bc7
siratee.k <siratee.k@linecorp.com>
2022-06-10 11:19:43 +0700

    feat: Make the RequestBox be collapsible

--- src/components/RequestBox/RequestBox.vue
+++ src/components/RequestBox/RequestBox.vue
@@ -2,23 +2,19 @@
   <div
     class="space-y-4 bg-green-100 m-2 rounded-xl p-3 hover:bg-green-200 duration-75"
   >
-    <div class="flex">
+    <div class="flex cursor-pointer" @click="toggleShowDetail">
       <div class="bg-green-500 rounded-xl text-lg p-1.5">GET</div>
       <p class="text-lg duration-100 font-medium text-left p-1.5 ml-1">
         <!-- Request URL -->
         /exampleA/exampleB/apiC
       </p>
 
-      <button
-        class="hover:bg-gray-300 hover:bg-opacity-50 duration-150 rounded-xl h-max w-max"
-      >
-        <span class="material-symbols-outlined align-middle">
-          expand_more
-        </span>
-      </button>
+      <span class="material-symbols-outlined align-middle m-1.5">
+        {{ isShowDetail ? "expand_less" : "expand_more" }}
+      </span>
     </div>
 
-    <TabWithIcons />
+    <TabWithIcons :class="isShowDetail ? '' : 'hidden'" />
   </div>
 </template>
 
@@ -35,8 +31,18 @@ export default defineComponent({
       required: true,
     },
   },
+  data() {
+    return {
+      isShowDetail: false,
+    };
+  },
   components: {
     TabWithIcons,
+  },
+  methods: {
+    toggleShowDetail() {
+      this.isShowDetail = !this.isShowDetail;
+    },
   },
 });
 </script>
